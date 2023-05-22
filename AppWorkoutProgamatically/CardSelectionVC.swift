b866760d794e2f1e20d88b92f65a74ef2d0f6e4e
saminshakya <saminshakya27@gmail.com>
2023-05-22 11:19:56 +0545

    created customize buttonVC

--- AppWorkoutProgamatically/CardSelectionVC.swift
+++ AppWorkoutProgamatically/CardSelectionVC.swift
@@ -11,21 +11,10 @@ class CardSelectionVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
         view.backgroundColor = .brown
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
