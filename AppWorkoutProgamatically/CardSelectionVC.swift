235123e8ae30765b6eb515cbaa57468f0e12558a
saminshakya <saminshakya27@gmail.com>
2023-05-22 10:58:38 +0545

    programatic ui created modified scene delegate

---
+++ AppWorkoutProgamatically/CardSelectionVC.swift
@@ -0,0 +1,31 @@
+//
+//  CardSelectionVC.swift
+//  AppWorkoutProgamatically
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 22/05/2023.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        view.backgroundColor = .brown
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
