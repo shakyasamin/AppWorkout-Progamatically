2dad87ec98412c031b20182519200b2a2e9d1508
saminshakya <saminshakya27@gmail.com>
2023-05-22 10:35:07 +0545

    project setup

---
+++ AppWorkoutProgamatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppWorkoutProgamatically
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 22/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
